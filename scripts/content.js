10a00a5e273aa5401fd49494ae879b0c8addf75d
Declan Blanchard <declanblanc+github@gmail.com>
2025-03-13 20:49:06 -0400

    Cleaned up a bit and added comments for clarity

--- scripts/content.js
+++ scripts/content.js
@@ -1,29 +1,23 @@
+// Locate the navbar link for the explore page and remove it.
 const exploreLink = document.querySelector(".link.explore");
 if (exploreLink) {
   exploreLink.innerHTML = ``;
 }
 
+// Removes most of the content on the main page where you can find links to other games.
 const siteBody = document.querySelector(".splash");
 if (siteBody) {
   siteBody.innerHTML = ``;
 }
 
-const linkTwo = document.querySelector(".site-nav");
-if (linkTwo) {
-  linkTwo.innerHTML = `<li><a id="project-create" href="/projects/editor/">Create</a></li><li><a href="/mystuff">Explore</a></li><li><a href="/ideas">Ideas</a></li><li class="last"><a href="/about/">About</a></li>`;
-}
-
+// Removes the search bar that enables searching for games
 const searchBar = document.querySelector(".col-sm-9");
 if (searchBar) {
   searchBar.innerHTML = ``;
 }
 
-if (window.location.href.includes("search")) {
+// If students navigate to the explore page from an external link, redirect them to their projects
+if (window.location.href.includes("search") || window.location.href.includes("explore")) {
   const redirectUrl = "https://scratch.mit.edu/mystuff/"; 
-  window.location.href = redirectUrl;
-}
-
-if (window.location.href.includes("explore")) {
-  const redirectUrl = "https://scratch.mit.edu/mystuff/";
   window.location.href = redirectUrl;
 }
